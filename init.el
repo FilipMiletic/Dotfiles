2a64f69d1d6b8210e5f438d7a31859cad68b8ef5
Filip Miletic <filip.miletic@me.com>
2016-06-22 20:27:10 +0200

    back to emacs

--- init.el
+++ init.el
@@ -3,14 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes )
- '(fringe-mode 2 nil (fringe))
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+	("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(fringe-mode 0 nil (fringe))
  '(linum-format " %d ")
  '(ns-use-native-fullscreen t)
  '(package-selected-packages
    (quote
-	(molokai-theme color-theme-sanityinc-tomorrow atom-one-dark-theme)))
+	(smooth-scrolling magit evil-leader alchemist zerodark-theme spaceline ## atom-one-dark-theme)))
  '(show-paren-delay 0.0)
+ '(sml/theme (quote respectful))
  '(spacemacs-theme-comment-bg nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -18,7 +22,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
 (setq user-full-name "Filip Miletic")
 (setq user-mail-adress "filip.miletic@me.com")
 
@@ -30,6 +33,7 @@
 ;; disable cocoa gui 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(global-hl-line-mode 1)
 ;; (menu-bar-mode -1)
 
 ;; selection mode modified
@@ -48,7 +52,6 @@
 (setq make-backup-files nil)
 
 ;; show matching brackets
-
 (show-paren-mode t)
 
 ;; melpa and marmalade package configs
@@ -69,19 +72,24 @@
 (set-default-font "Menlo 12")
 
 ;; theme setting
-(load-theme 'spolsky t)
-
-;; configure and enable default Spaceline theme
-(load "spaceline-config")
-(require 'spaceline-config)
-(spaceline-spacemacs-theme)
+(load-theme 'molokai t)
+;(load-theme 'zerodark t)
 
 ;; enabling EVIL mode
 (require 'evil)
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer)
 (evil-mode t)
-;;(goto-last-change t)
-;;(goto-last-change-reverse t)
 
+
+;; smart mode line setup
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
 
 ;; disable 3D emacs drawing of objects
 (set-face-attribute 'mode-line nil :box nil)
@@ -137,16 +145,21 @@
 	   (lambda (file) (princ (format "(load \"%s\")\n" file)))
 	   (get-loads-from-*Messages*)))
 
-(defun go-mode-setup()
-  (go-eldoc-setup)
-  (setq compile-command "go build -v && go test -v && go vet")
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save) )
-(add-hook 'go-mode-hook 'go-mode-setup)
+;;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-(require 'auto-complete-config)
-(require 'auto-complete-clang)
-(require 'go-autocomplete)
-
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
