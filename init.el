93cabcc74f806c366962d95c289c81ad855d2e0a
Filip Miletic <filip.miletic@me.com>
2016-06-25 16:29:55 +0200

    emacs server set up

--- init.el
+++ init.el
@@ -116,7 +116,15 @@
 	  (eval-buffer)))))
 
 ;; Allow this emacs process to be a server for client processes.
-;(server-start)
+(server-start)
+
+(defadvice handle-delete-frame (around my-handle-delete-frame-advice activate)
+  "Hide Emacs instead of closing the last frame."
+  (let ((frame (posn-window (event-start event)))
+		(numfrs (length(frame-list))))
+	(if (> numfrs 1)
+		ad-do-it
+	  (do-applescript "tell application \"System Events\" to tell process \"Emacs\" to set visible to false"))))
 
 (use-package powerline
   :ensure t
