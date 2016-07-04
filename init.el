c9811b4c8109e18615406a53a396b5b962796f02
Filip Miletic <filip.miletic@me.com>
2016-07-04 17:08:08 +0200

    updating variables

--- init.el
+++ init.el
@@ -37,9 +37,12 @@
 	  `((".*" ,temporary-file-directory t)))
 
 (defun my-minibuffer-setup-hook ()
+  "Defining setup hook for GC threshold."
   (setq gc-cons-threshold most-positive-fixnum))
 
 (defun my-minibuffer-exit-hook ()
+  "Defining exit hook for GC threshold."
+  (defvar gc-cons-threshold)
   (setq gc-cons-threshold 800000))
 
 (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
@@ -63,6 +66,7 @@
 (delete-selection-mode 1)
 (transient-mark-mode 1)
 (blink-cursor-mode 1)
+(defvar linum-format)
 (setq ring-bell-function #'ignore
 	  inhibit-startup-screen t
 	  echo-keystrokes 0.1
@@ -90,14 +94,19 @@
   (load-theme 'darkokai t))
 
 ;; System setup
+(defvar delete-old-version)
 (setq delete-old-version t)
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
 
 (defun copy-from-osx ()
+  "Defining shell command to str."
+  (defvar shell-command-to-string)
   (shell-command-to-string "pbpaste"))
 
 (defun paste-to-osx (text &optional push)
+  "Var process connectio type for paste-to-osx."
+  (defvar process-connection-type)
   (let ((process-connection-type nil))
 	(let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
 	  (process-send-string proc text)
@@ -169,18 +178,13 @@
 		("C-c s w" . sp-rewrap-sexp))
   :diminish (smartparens-mode))
 
-;; Interactivley Do Things for buffers and mode line navigation
-(use-package ido
-  :config
-  (setq ido-enable-flex-matching t)
-  (ido-everywhere t)
-  (ido-mode 1))
-
 ;; Highlight current line
 (use-package hl-line
   :init (global-hl-line-mode 1))
 
 ;; C code style
+(defvar c-default-style)
+(defvar  c-basic-offset)
 (setq c-default-style "k&r"
 	  c-basic-offset 4
 	  indent-tabs-mode t)
@@ -201,7 +205,8 @@
 ;; If a file is changed outside emacs, load those changes in buffers
 (global-auto-revert-mode t)
 
-(use-package company                    ;; Graphical (auto-)completion
+;; Graphical (auto-)completion
+(use-package company
   :ensure t
   :init (global-company-mode)
   :config
@@ -239,8 +244,12 @@
 			  ("<tab>" . helm-execute-persistent-action)
 			  ("C-i" . helm-execute-persistent-action))
   :config (progn
-			(setq helm-buffers-fuzzy-matchin t)
+			(defvar helm-buffers-fuzzy-matching)
+			(setq helm-buffers-fuzzy-matching t)
 			(helm-mode 1)
+			(defvar helm-ff-search-library-in-sexp)
+			(defvar helm-ff-file-name-history-use-recentf)
+			(defvar helm-ag-fuzzy-match)
 			(setq helm-split-window-in-side-p t
 				  helm-buffers-fuzzy-matching t
 				  helm-move-to-line-cycle-in-source t
@@ -249,7 +258,8 @@
 				  helm-ag-fuzzy-match t)
 
 			(substitute-key-definition 'find-tag 'helm-etags-select global-map)
-			(setq projectile-completition-system 'helm))
+			(defvar projectile-completion-system)
+			(setq projectile-completion-system 'helm))
   (add-to-list 'display-buffer-alist
 			   `(,(rx bos "*helm" (* not-newline) "*" eos)
 				 (display-buffer-reuse-window display-buffer-in-side-window)
@@ -277,29 +287,13 @@
   :after flycheck)
 
 (use-package multiple-cursors
-  :ensure t
-  :bind (("C-c o <SPC>" . mc/vertical-align-with-space)
-		 ("C-c o a"     . mc/vertical-align)
-		 ("C-c o e"     . mc/mark-more-like-this-extended)
-		 ("C-c o h"     . mc/mark-all-like-this-dwim)
-		 ("C-c o l"     . mc/edit-lines)
-		 ("C-c o n"     . mc/mark-next-like-this)
-		 ("C-c o p"     . mc/mark-previous-like-this)
-		 ("C-c o r"     . vr/mc-mark)
-		 ("C-c o C-a"   . mc/edit-beginnings-of-lines)
-		 ("C-c o C-e"   . mc/edit-ends-of-lines)
-		 ("C-c o C-s"   . mc/mark-all-in-region))
-  :config
-  (setq mc/mode-line
-		;; Simplify the MC mode line indicator
-		'(:propertize (:eval (concat " " (number-to-string (mc/num-cursors))))
-					  face font-lock-warning-face)))
+  :ensure t)
 
 (use-package projectile
   :ensure t
   :bind (("C-x p" . projectile-presp-switch-project))
   :config
-  (setq projectile-completition-system 'helm)
+  (setq projectile-completion-system 'helm)
   (projectile-global-mode)
   (helm-projectile-on)
   (setq projectile-enable-caching nil)
@@ -311,47 +305,13 @@
   (helm-projectile-on))
 
 (use-package elixir-mode
-  :load-path  "~/.emacs.d/emacs-elixir/"
-  :config (progn
-			(defun my-elixir-do-end-close-action (id action context)
-			  (when (eq action 'insert)
-				(newline-and-indent)
-				(forward-line -1)
-				(indent-according-to-mode)))
-
-			(sp-with-modes '(elixir-mode)
-			  (sp-local-pair "do" "end"
-							 :when '(("SPC" "RET"))
-							 :post-handlers '(:add my-elixir-do-end-close-action)
-							 :actions '(insert)))))
+  :ensure t
+  :commands elixir-mode
+  :config (add-hook 'elixir-mode-hook 'alchemist-mode))
 
 (use-package alchemist
-  :defer 1
-  :load-path "~/.emacs.d/alchemist.el/"
-  :bind (:map alchemist-iex-mode-map
-			  ("C-d" . windmove-right)
-			  :map alchemist-mode-map
-			  ("M-w" . alchemist-goto-list-symbol-definitions))
-  :config (progn
-			;;			(setq alchemist-goto-elixir-source-dir "~/Developer/elixir/")
-			;;			(setq alchemist-goto-erlang-source-dir "~/Developer/otp/")
-			(defun phil-alchemist-mode-hook ()
-			  (tester-init-test-run #'alchemist-mix-test-file "_test.exs$")
-			  (tester-init-test-suite-run #'alchemist-mix-test))
-			(add-hook 'alchemist-mode-hook 'phil-alchemist-mode-hook)
-
-			;; Display alchemist buffer on top
-			(add-to-list 'display-buffer-alist
-						 `(,(rx bos (or  "*alchemist test report*"
-										 "*alchemist mix*"
-										 "*alchemist help*"
-										 "*alchemist elixir*"
-										 "*alchemist elixirc*"))
-						   (display-buffer-reuse-window
-							display-buffer-in-side-window)
-						   (reusable-frames . visible)
-						   (side . right)
-						   (window-width . 0.5)))))
+  :ensure t
+  :commands (alchemist-mode))
 
 (use-package erlang
   :ensure t
@@ -364,22 +324,16 @@
   :if (eq system-type 'darwin)
   :config
   (setq ns-pop-up-frames nil
-
 		mac-option-modifier 'meta
 		mac-command-modifier 'meta
 		mac-right-command-modifier 'left
 		mac-right-option-modifier 'none
 		mac-function-modifier 'hyper))
 
-(use-package  exec-path-from-shell
+(use-package exec-path-from-shell
   :ensure t
-  :if (and (eq system-type 'darwin) (display-graphic-p))
   :config
-  (progn
-	(when (string-match-p "/zsh%" (getenv "SHELL"))
-	  ;; (setq exec-path-from-shell-arguments '("-l")))
-
-	  (exec-path-from-shell-initialize))))
+	  (exec-path-from-shell-initialize))
 
 (use-package flycheck
   :ensure t
@@ -404,6 +358,28 @@
 		  (add-hook hook #'whitespace-cleanup-mode))
   :diminish (whitespace-cleanup-mode))
 
+;; Org-mode settings
+(defvar org-log-done)
+(defvar org-todo-keywords)
+(defvar org-todo-keywords-face)
+(setq org-log-done t
+	  org-todo-keywords '((sequence "TODO" "INPROGRESS" "DONE"))
+	  org-todo-keywords-face '(("INPROGRESS" . ("foreground " "blue" : weight bold))))
+
+(add-hook 'org-mode-hook
+		  (lambda ()
+			(flyspell-mode)))
+
+;; disable flyspell welcome message
+(defvar flyspell-issue-welcome-flag)
+(setq flyspell-issue-welcome-flag nil)
+
+;; change between normal and relative
+;; line numbering by C-c ln
+(use-package linum-relative
+  :ensure t
+  :bind ("C-c l n" . linum-relative-toggle))
+
 ;; ssh-config
 (use-package ssh-config-mode
   :mode ((".ssh/config\\'" . ssh-config-mode)
@@ -411,14 +387,14 @@
 		 ("known_hosts\\'" . ssh-known-hosts-mode)
 		 ("authorized_keys2?\\'" . ssh-autorized-keys-mode)))
 
-;; json
-(use-package json-mode
-  :mode "\\.json$"
-  :config (setq js-indent-level 4))
-
 ;; markdown
 (use-package markdown-mode
-  :mode ("\\.markdown\\'" "\\.mk?d\\'" "\\.text\\'"))
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+		 ("\\.md\\'" . markdown-mode)
+		 ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 ;; toml
 (use-package toml-mode
@@ -427,12 +403,6 @@
 ;; yaml
 (use-package yaml-mode
   :mode "\\.ya?ml\'")
-
-(add-hook 'org-mode-hook
-		  (lambda ()
-			(org-bullets-mode t)))
-
-(setq org-ellipsis "⤵")
 
 ;;; provide init package
 (provide 'init)
