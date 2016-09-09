b229d00720fb26210679572539f2778033ae6ac7
Filip Miletic <filip.miletic@me.com>
2016-09-09 18:30:33 +0200

    updating emacs config

--- init.el
+++ init.el
@@ -1,17 +1,14 @@
 ;;; package --- Summary
 ;; exec-path-from-shell
 ;; flycheck
-;; theme: apropospriate-theme; firebelly;
-;; nlinum
-;; sml
-;; ido: flx + ubiquitus + vertical
+;; ido: flx + uqiuitus + vertical
 ;; company
 ;; agressive indent
 
 ;;; Commentary:
-;; My personal Emacs configuration.
 
 ;;; Code:
+
 ;; Setup package system
 (require 'package)
 (add-to-list 'package-archives
@@ -27,40 +24,60 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Global Configuration
-(setq mac-option-modifier nil)
-(setq mac-command-modifier 'meta)
-(setq load-prefer-newer t)
-(setq gc-cons-threshold 25000000)
-(setq-default tab-width 4)
-(setq ring-bell-function 'ignore)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq inhibit-splash-screen t)
-(setq initial-scratch-message nil)
-(setq-default truncate-lines t)
+;; Load paths
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(add-to-list 'load-path "~/.emacs.d/themes/doom-theme/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+
+;; Modes
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq mac-allow-anti-aliasing t)
-(line-number-mode 1)
-(column-number-mode 1)
-(setq require-final-newline t)
-(prefer-coding-system 'utf-8)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll one line at a time
-(setq scroll-conservatively 10000)
-(setq scroll-margin 3)
+(column-number-mode t)
 (show-paren-mode 1)
-(setq ns-pop-up-frames nil)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-(setq sentence-end-double-space nil)
-(add-to-list 'load-path "~/.emacs.d/themes/emacs-doom-theme/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-doom-theme/")
+(global-auto-revert-mode 1)
+(global-prettify-symbols-mode 1)
+(global-hl-line-mode 1)
 
+;; Global configuration
+
+(setq mac-option-modifier nil
+      mac-command-modifier 'meta
+      load-prefer-newer t
+      gc-cons-threshold 25000000
+      ring-bell-function 'ignore
+      inhibit-splash-screen t
+      initial-scratch-message nil
+      require-final-newline t
+      mouse-wheel-scroll-amount '(1 ((shift) .1))
+      mouse-wheel-progressive-speed nil
+      mouse-wheel-follow-mouse 't
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-margin 3
+      fringes-outside-margins t
+      ns-pop-up-frames nil
+      make-backup-files nil
+      backup-inhibited nil
+      auto-save-default nil
+      create-lockfiles nil
+      sentence-end-double-space nil)
+
+
+;; Indentation
+
+(setq-default indent-tabs-mode nil
+	      tab-width 2
+	      c-basic-offset 4
+        fill-column 80)
+	      
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Don't close Emacs via GUI command
 (defadvice handle-delete-frame (around my-handle-delete-frame-advice activate)
   "Hide Emacs instead of closing the last frame."
   (let ((frame (posn-window (event-start event)))
@@ -69,46 +86,29 @@
 		ad-do-it
 	  (do-applescript "tell application \"System Events\" to tell process \"Emacs\" to set visible to false"))))
 
-(defvar c-default-style)
-(defvar c-basic-offset)
-(setq c-default-style "k&r"
-	  c-basic-offset 4
-	  indent-tabs-mode t)
+;; Theme
+(setq custom-safe-themes t)
+(require 'doom-theme)
+(load-theme 'doom-one t)
 
-(global-prettify-symbols-mode t)
-(global-auto-revert-mode t)
-(global-hl-line-mode t)
-
-;; Exec path from shell
 (use-package exec-path-from-shell
   :ensure t
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (setq exec-path-from-shell-check-startup-files nil))
 
 (use-package flycheck
   :ensure t
   :defer 2
-  :config
-  (global-flycheck-mode 1))
+  :config (global-flycheck-mode 1))
 
-(setq custom-safe-themes t)
-(load-theme 'doom-dark)
-;;(use-package spacemacs-theme
-;;:ensure t
-;;:init (load-theme 'spacemacs-dark ))
-
-(use-package nlinum
+(use-package ido-ubiquitous
   :ensure t
-  :config
-  (global-nlinum-mode 1))
+  :config (ido-ubiquitous-mode t))
 
-(use-package smart-mode-line
+(use-package flx-ido
   :ensure t
-  :config
-  (progn
-    (setq sml/no-confirm-load-theme t)
-    (setq sml/theme 'respectful)
-    (add-hook 'after-init-hook #'sml/setup)))
+  :config (setq ido-use-faces nil))
 
 (use-package ido
   :ensure t
@@ -117,26 +117,11 @@
     (ido-mode t)
     (ido-everywhere t)
     (flx-ido-mode t)
-    (setq ido-enable-flex-matching t)
-    (setq ido-use-faces nil)))
-
-(use-package ido-ubiquitous
-  :ensure t
-  :config
-  (ido-ubiquitous-mode t))
-
-(use-package flx-ido
-  :ensure t
-  :config
-  (flx-ido-mode t)
-  (setq ido-use-faces nil))
+    (setq ido-enable-flex-matching t)))
 
 (use-package ido-vertical-mode
   :ensure t
-  :config
-  (progn
-    (ido-vertical-mode 1)
-    (setq ido-vertical-define-keys #'C-n-and-C-p-only)))
+  :config (ido-vertical-mode 1))
 
 (use-package company
   :ensure t
@@ -148,11 +133,11 @@
         company-tooltip-limit 10)
   (global-company-mode 1))
 
-(use-package aggressive-indent
-  :ensure t
-  :config
-  (global-aggressive-indent-mode t))
+(use-package multiple-cursors
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)))
+
+(drag-stuff-global-mode t)
 
 (provide 'init)
-
 ;;; init.el ends here
